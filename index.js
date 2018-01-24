d3c96de92852224e1ad12fa0a852ae4eda935a95
Remya Jacob <remyaabi@gmail.com>
2018-01-24 20:53:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var selectorValue="main.#app li";
+function getFirstSelector(selectorValue){
+  return document.querySelector(selectorValue);
+  
+}
+
+function nestedTarget(){
+  var element='div';
+  var divNested=document.getElementById('app').querySelectorAll('div#nested');
+  var divTargetArray=[];
+  for(let i=0;i<divNested.length;i++){
+       var divTarget=divNested[i].child.querySelectorAll('.target');
+       divTargetArray.push(divTarget);
+  }
+  return divTargetArray;
+}
+ function increaseRankBy(n){
+  var ranked_list_value=document.querySelectorAll("ul.ranked-list li");
+    for(let i=0; i<ranked_list_value.length;i++){
+       console.log("ranked_list_value : " + ranked_list_value[i]);
+       console.log("ranked_list_value.innerText : " + ranked_list_value[i].innerHTML);
+       var value=parseInt(ranked_list_value[i].innerHTML) + n;
+       console.log("value : "+value)
+       ranked_list_value[i].innerHTML=value;
+      }
+     }
+     
+   function deepestChild() {
+     var childNodeOfDiv=document.querySelectorAll('main#app');
+     for(let i=0; i<childNodeOfDiv;i++)
+      {
+         while(childNodeOfDiv[i].child){
+           
+         }
+         }
+      }
+    
